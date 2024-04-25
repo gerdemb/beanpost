c5b71915cfc66de511c3a516d8b8da53d2917ec5
Ben Gerdemann <github@ibeni.net>
2024-04-25 11:59:58 -0300

    Make the tests a little less noisy

--- tests/reset_schema.sql
+++ tests/reset_schema.sql
@@ -1,3 +1,5 @@
+SET client_min_messages = 'ERROR';
+
 DROP SCHEMA public CASCADE;
 
 \ir ../schema.sql
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,9 +3,9 @@
 # Change to the directory where the script is located
 cd "$(dirname "$0")"
 
-psql $DATABASE_URL -f reset_schema.sql
+psql $DATABASE_URL --quiet --file=reset_schema.sql
 ../beanpost-import.py test.beancount $DATABASE_URL
-psql $DATABASE_URL -f tests.sql | diff - expected_output.txt
+psql $DATABASE_URL --quiet --file=tests.sql | diff - expected_output.txt
 
 # Check the exit status of `diff`
 if [ $? -eq 0 ]; then
